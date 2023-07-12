83c8e92befd1327cdc42870afffb6fe8b49c5994
cesarcor <cesarcorrechel@gmail.com>
2023-07-12 14:03:57 -0600

    add misc. layout elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import styles from './page.module.scss';
+import Hero from './components/hero/hero';
 
 export default function Home() {
-	return <main className={styles.main}></main>;
+	return (
+		<section className='site-section'>
+			<Hero />
+		</section>
+	);
 }
