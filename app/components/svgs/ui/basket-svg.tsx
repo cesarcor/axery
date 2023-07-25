02079f1a4527321fddcb2479ecc322259ba4e9cb
cesarcor <cesarcorrechel@gmail.com>
2023-07-24 18:01:32 -0600

    add header acts. svgs

---
+++ app/components/svgs/ui/basket-svg.tsx
@@ -0,0 +1,21 @@
+function BasketSVG() {
+	return (
+		<svg
+			width='15px'
+			height='19px'
+			viewBox='0 0 15 19'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M3.848 5.265V3.799C3.848 1.707 5.476 0 7.5 0c2.023 0 3.651 1.707 3.651 3.8v1.465h2.525c.24 0 .439.186.455.426L15 18.512a.46.46 0 01-.425.488H.456A.456.456 0 010 18.543v-.03L.868 5.69a.458.458 0 01.455-.428l2.525.002zm1.37 0h4.565V3.799c0-1.346-1.023-2.426-2.283-2.426-1.261 0-2.282 1.08-2.282 2.426v1.466zM1.433 17.629h12.133l-.742-10.99H2.177l-.744 10.99z'
+				transform='translate(-131 -90) translate(131 90)'
+				fill='#FFF'
+				fillRule='nonzero'
+				stroke='none'
+				strokeWidth={1}
+			/>
+		</svg>
+	);
+}
+
+export default BasketSVG;
---
+++ app/components/svgs/ui/heart-svg.tsx
@@ -0,0 +1,21 @@
+function HeartSVG() {
+	return (
+		<svg
+			width='18'
+			height='18px'
+			viewBox='0 0 13.4416561 12'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M3.533 0C1.419 0 0 1.711 0 3.824 0 6.222 3.35 9.087 6.22 11c2.871-1.913 6.182-4.824 6.221-7.176C12.476 1.711 11.108 0 8.994 0 8.092 0 7.177.477 6.22 1.433 5.263.477 4.435 0 3.533 0z'
+				transform='translate(-193 -107) translate(193 107)'
+				stroke='#FFF'
+				strokeWidth={1}
+				fill='none'
+				fillRule='evenodd'
+			/>
+		</svg>
+	);
+}
+
+export default HeartSVG;
