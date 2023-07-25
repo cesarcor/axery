0711f1a80ebc866d7b7b5025c6e4ede3df06866d
cesarcor <cesarcorrechel@gmail.com>
2023-07-24 18:00:27 -0600

    add header

---
+++ app/components/header/header.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import styles from './header.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+import Search from '../common-ui/search/search';
+import WishlistWidget from './wishlist-widget/wishlist-widget';
+import CartWidget from './cart-widget/cart-widget';
+import SearchBtn from './search-btn/search-btn';
+import MobileMenu from './mobile-menu/mobile-menu';
+import useScrollDirection from '../../hooks/useScrollDirection';
+
+const Header = () => {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+	const scrollDirectionUp = useScrollDirection();
+
+	const toggleMobileMenu = () => {
+		setIsMobileMenuOpen(!isMobileMenuOpen);
+	};
+
+	useEffect(() => {
+		if (isMobileMenuOpen) {
+			document.body.style.position = 'fixed';
+		} else {
+			document.body.style.position = 'static';
+		}
+
+		return () => {
+			document.body.style.position = 'static';
+		};
+	}, [isMobileMenuOpen]);
+
+	return (
+		<>
+			<header
+				className={`${styles.header} ${
+					scrollDirectionUp ? styles.retreat : styles.down
+				}`}
+			>
+				<div className={styles.container}>
+					<div className={styles.search_col_mobile}>
+						<SearchBtn />
+					</div>
+					<div className={styles.logo_col}>
+						<Image src='/images/logo.png' alt='Axery' width={100} height={40} />
+					</div>
+					<div className={styles.menu_col}>
+						<button
+							className={`${styles.hamburger} ${
+								isMobileMenuOpen ? styles.isActive : ''
+							}`}
+							onClick={toggleMobileMenu}
+						>
+							<div className={styles.line}></div>
+							<div className={styles.line}></div>
+							<div className={styles.line}></div>
+						</button>
+						<nav className={styles.desktop_nav}>
+							<ul>
+								<li>
+									<Link href='/'>Home</Link>
+								</li>
+								<li>
+									<Link href='/'>Products</Link>
+								</li>
+								<li>
+									<Link href='/'>Blog</Link>
+								</li>
+								<li>
+									<Link href='/'>Contact</Link>
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<div className={styles.actions_col}>
+						<Search />
+						<WishlistWidget />
+						<CartWidget />
+					</div>
+				</div>
+			</header>
+			<MobileMenu menuIsOpen={isMobileMenuOpen} />
+		</>
+	);
+};
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import './styles/globals.scss';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
+import Header from './components/header/header';
 
-const open_sans = Open_Sans({ subsets: ['latin'], weight: ['300', '400'] });
+const open_sans = Open_Sans({
+	subsets: ['latin'],
+	weight: ['300', '400', '700'],
+});
 
 export const metadata: Metadata = {
 	title: 'Axery - Elegant Leather Backpacks',
@@ -17,6 +21,7 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={open_sans.className}>
+				<Header />
 				<main className='site-main'>{children}</main>
 			</body>
 		</html>
