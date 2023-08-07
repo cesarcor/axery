2c06f2beacdb85f2e0a09b228b1778041c872c34
cesarcor <cesarcorrechel@gmail.com>
2023-08-07 15:12:19 -0600

    add page sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 import styles from './page.module.scss';
 import Hero from './components/hero/hero';
+import Benefits from './components/benefits/benefits';
+import AboutSection from './components/sections/about-section/about-section';
+import FeaturedProducts from './components/sections/featured-products/featured-products';
 
 export default function Home() {
 	return (
-		<section className='site-section'>
-			<Hero />
-		</section>
+		<>
+			<section className='site-section'>
+				<Hero />
+			</section>
+			<section className='site-section'>
+				<Benefits />
+			</section>
+			<section className='site-section'>
+				<AboutSection />
+			</section>
+			<section className='site-section'>
+				<FeaturedProducts />
+			</section>
+		</>
 	);
 }
