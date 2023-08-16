7ff0f21f2b425e00f4662a2cd67c4658ce79802c
cesarcor <cesarcorrechel@gmail.com>
2023-08-16 12:44:43 -0600

    add search popup opening functionality

--- app/components/header/search-btn/search-btn.tsx
+++ app/components/header/search-btn/search-btn.tsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import styles from './search-btn.module.scss';
 import SearchSVG from '../../svgs/ui/search-svg';
+import Search from '../../common-ui/search/search';
 
 const SearchBtn = () => {
+	const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false);
+
+	const toggleSearch = () => {
+		setIsSearchOpen(!isSearchOpen);
+	};
+
 	return (
-		<button className={styles.search_btn}>
-			<SearchSVG />
-		</button>
+		<>
+			<button
+				className={`${styles.search_btn} ${isSearchOpen ? styles.active : ''}`}
+				onClick={toggleSearch}
+			>
+				<SearchSVG />
+			</button>
+			{isSearchOpen && (
+				<div className={styles.mobile_search_popup}>
+					<Search />
+				</div>
+			)}
+		</>
 	);
 };
 
