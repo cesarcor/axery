f9d46288936ffabd01ed0dd8beb45fe004f57c80
cesarcor <cesarcorrechel@gmail.com>
2023-08-16 12:31:34 -0600

    handle dynamic input resizing + modal visibility

--- app/components/common-ui/search/search.tsx
+++ app/components/common-ui/search/search.tsx
@@ -1,11 +1,40 @@
+import React, { useState } from 'react';
 import styles from './search.module.scss';
 import SearchSVG from '../../svgs/ui/search-svg';
+import SearchSuggestionsModal from '../search-suggestions-modal/search-suggestions-modal';
 
 const Search = () => {
+	const [searchQuery, setSearchQuery] = useState<string>('');
+	const [isFocused, setIsFocused] = useState<boolean>(false);
+
+	const handleFocus = () => {
+		setIsFocused(true);
+	};
+
+	const handleBlur = () => {
+		setIsFocused(false);
+	};
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const inputValue = event.target.value;
+		setSearchQuery(inputValue);
+	};
+
 	return (
-		<div className={styles.search}>
-			<input type='text' placeholder='Search' />
-			<SearchSVG />
+		<div className={styles.search_container}>
+			<div className={styles.search}>
+				<input
+					type='text'
+					placeholder='Search'
+					className={`${styles.search_input}`}
+					value={searchQuery}
+					onChange={handleInputChange}
+					onFocus={handleFocus}
+					onBlur={handleBlur}
+				/>
+				<SearchSVG />
+			</div>
+			{searchQuery.length > 0 && isFocused && <SearchSuggestionsModal />}
 		</div>
 	);
 };
