d3cdd5f34ab8c82f1ff13cc9db7e1e8eff08a12d
cesarcor <cesarcorrechel@gmail.com>
2023-08-21 22:49:01 -0600

    add BlogItem comp.

---
+++ app/components/blog/blog-item/blog-item.tsx
@@ -0,0 +1,36 @@
+import styles from './blog-item.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const BlogItem = () => {
+	return (
+		<article className={styles.blog_item}>
+			<div className={styles.container}>
+				<div className={styles.img_col}>
+					<figure className={styles.fig}>
+						<Image
+							src='/images/l_handbag.jpg'
+							alt='lorem ipsum'
+							width='300'
+							height='250'
+						/>
+					</figure>
+					<span className={styles.date}>12.03</span>
+				</div>
+				<div className={styles.content}>
+					<h3 className={styles.title}>
+						<Link href='/' className='text-white'>
+							Lorem ipsum is simply dummy text of the printing industry
+						</Link>
+					</h3>
+					<p className={styles.snippet}>
+						Lorem ipsum is simply dummy text of the printing industry. Lorem
+						ipsum has been the industrys standard dummy text
+					</p>
+				</div>
+			</div>
+		</article>
+	);
+};
+
+export default BlogItem;
