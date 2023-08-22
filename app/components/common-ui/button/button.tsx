2bf149913940e026e9efaf795767fac0c005be66
cesarcor <cesarcorrechel@gmail.com>
2023-08-22 10:06:09 -0600

    add arrow svg

--- app/components/common-ui/button/button.tsx
+++ app/components/common-ui/button/button.tsx
@@ -1,14 +1,22 @@
 import styles from './button.module.scss';
+import ArrowSVG from '../../svgs/arrow-svg';
 
 interface ButtonProps {
 	text: string;
 	link: string;
+	buttonType?: 'solid' | 'line';
 }
 
-const Button = ({ text, link }: ButtonProps) => {
+const Button = ({ text, link, buttonType = 'solid' }: ButtonProps) => {
+	const buttonTypeClassname =
+		buttonType === 'solid' ? styles.solid : styles.line;
+
 	return (
-		<a href={link} className={styles.btn}>
-			{text}
+		<a href={link} className={`${styles.btn} ${buttonTypeClassname}`}>
+			<span className={styles.btn_content}>
+				<span>{text}</span>
+				<ArrowSVG />
+			</span>
 		</a>
 	);
 };
