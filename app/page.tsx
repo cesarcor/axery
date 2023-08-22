6d13ebf3189269d20d0b04f23a6a06247d4fc9ee
cesarcor <cesarcorrechel@gmail.com>
2023-08-21 22:52:04 -0600

    add quote and blog sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Hero from './components/hero/hero';
 import Benefits from './components/benefits/benefits';
 import AboutSection from './components/sections/about-section/about-section';
 import FeaturedProducts from './components/sections/featured-products/featured-products';
+import Quote from './components/quote/quote';
+import BlogSection from './components/blog/blog-section/blog-section';
 
 export default function Home() {
 	return (
 		<>
-			<section className='site-section'>
+			<section className='site-section no-padding-bottom'>
 				<Hero />
 			</section>
 			<section className='site-section'>
@@ -18,6 +20,12 @@ export default function Home() {
 			</section>
 			<section className='site-section'>
 				<FeaturedProducts />
+			</section>
+			<section className='site-section'>
+				<Quote />
+			</section>
+			<section className='site-section'>
+				<BlogSection />
 			</section>
 		</>
 	);
